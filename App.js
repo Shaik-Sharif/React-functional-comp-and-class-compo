c7d5c83c4bffe0713aa16dc589c1c588a5d02daf
shaik Nagur sharif <136973778+Shaik-Sharif@users.noreply.github.com>
2024-05-24 11:20:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div>
+//     <h1>your in the game zone </h1>
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//           <h1>Shark tank Sharif</h1>
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//     </div>
+//   );
+// }
+
+export let person = {
+  name : 'john',
+  age:27,
+  Location : 'AP'
+}
+export let x = 10
+
+// export default App;
